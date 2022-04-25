9333244c10dd81bb2f553fe43746a31767672ec9
echo-g <1094380273@qq.com>
2022-04-25 15:36:33 +0800

    log statis

--- guide/log/statis.py
+++ guide/log/statis.py
@@ -13,38 +13,56 @@ from pm4py.statistics.traces.generic.log import case_statistics
 from pm4py.statistics.start_activities.log.get import get_start_activities
 from pm4py.statistics.end_activities.log.get import get_end_activities
 
-log = xes_importer('../statics/log/running-example.xes')
 
-trace_count = len(log)
-all_trace_attributes = get_all_trace_attributes_from_log(log)
-print("日志共有%d条迹，有%d种属性" % (trace_count, len(all_trace_attributes)), all_trace_attributes)
+def base_info(log):
+    trace_count = len(log)
+    all_trace_attributes = get_all_trace_attributes_from_log(log)
+    print("日志共有%d条迹，有%d种属性" % (trace_count, len(all_trace_attributes)), all_trace_attributes)
 
-event_count = 0
-for trace in log:
-    event_count += len(trace)
-all_event_attributes = get_all_event_attributes_from_log(log)
-print("日志共有%d个事件，有%d种属性" % (event_count, len(all_event_attributes)), all_event_attributes)
+    event_count = 0
+    for trace in log:
+        event_count += len(trace)
+    all_event_attributes = get_all_event_attributes_from_log(log)
+    print("日志共有%d个事件，有%d种属性" % (event_count, len(all_event_attributes)), all_event_attributes)
 
-activity_attr_value = get_attribute_values(log, 'concept:name')
-activity_attr_value_count = len(activity_attr_value)
-print("日志共有%d种活动" % activity_attr_value_count, list(activity_attr_value.keys()))
+    activity_attr_value = get_attribute_values(log, 'concept:name')
+    activity_attr_value_count = len(activity_attr_value)
+    print("日志共有%d种活动" % activity_attr_value_count, list(activity_attr_value.keys()))
 
-variants = case_statistics.get_variant_statistics(log)
-variant_count = len(variants)
-print("日志共有%d种不同的迹" % variant_count, variants)
+    variants = case_statistics.get_variant_statistics(log)
+    variant_count = len(variants)
+    print("日志共有%d种不同的迹" % variant_count, variants)
 
-start_activities=get_start_activities(log)
-print("开始活动",list(start_activities.keys()))
-end_activities=get_end_activities(log)
-print("结束活动",list(end_activities.keys()))
+    start_activities = get_start_activities(log)
+    print("开始活动", list(start_activities.keys()))
+    end_activities = get_end_activities(log)
+    print("结束活动", list(end_activities.keys()))
 
-#todo
-print(get_events_distribution(log))
 
-# 有多少活动发生了重复
-from pm4py.statistics.rework.cases.log.get import apply
-aa=apply(log)
-for key ,val in aa.items():
-    print(key,val)
-for variant in variants:
-    print(variant)
+def cycle_info(log):
+    # 每个案例中有多少活动发生了重复
+    from pm4py.statistics.rework.cases.log.get import apply as get_rework_case
+    aa = get_rework_case(log)
+    print(aa)
+    # 日志中活动重复发生了多少次
+    from pm4py.statistics.rework.log.get import apply as get_rework_log
+    bb = get_rework_log(log)
+    print(bb)
+
+def time_info(log):
+    from pm4py.statistics.sojourn_time.log.get import apply
+
+    from pm4py.statistics.traces.cycle_time.log.get import apply
+
+    from pm4py.statistics.passed_time.log.algorithm import apply
+
+
+if __name__ == '__main__':
+    # log = xes_importer('../statics/log/running-example.xes')
+    log = xes_importer('../statics/log/receipt.xes')
+    # base_info(log)
+    # cycle_info(log)
+
+    time_info(log)
+    # todo
+    # print(get_events_distribution(log))
--- guide/log/tes.py
+++ guide/log/tes.py
@@ -1,3 +1,33 @@
 from pm4py.objects.log.importer.xes.importer import apply as xes_importer
-log = xes_importer('create_log.xes')
-print(log)
+
+# log = xes_importer('../statics/log/running-example.xes')
+log = xes_importer('../statics/log/receipt.xes')
+# print(log)
+from pm4py.algo.transformation.log_to_features import algorithm as log_to_features
+
+data, feature_names = log_to_features.apply(log)
+print(feature_names)
+feature_set=[]
+for i in feature_names:
+    feature_set.append(i.split('@')[0])
+print(set(feature_set))
+
+import pandas as pd
+
+df = pd.DataFrame(data, columns=feature_names)
+from sklearn.decomposition import PCA
+
+pca = PCA(n_components=5)
+df2 = pd.DataFrame(pca.fit_transform(df))
+print(df2)
+
+import os
+import pm4py
+from pm4py.algo.transformation.log_to_features.util import locally_linear_embedding
+from pm4py.visualization.graphs import visualizer
+
+# log = pm4py.read_xes(os.path.join("tests", "input_data", "receipt.xes"))
+x, y = locally_linear_embedding.apply(log)
+gviz = visualizer.apply(x, y, variant=visualizer.Variants.DATES,
+                        parameters={"title": "Locally Linear Embedding", "format": "svg", "y_axis": "Intensity"})
+visualizer.view(gviz)
